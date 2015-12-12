66c2f95f53ff6eb00c01e5e5c70247944070e30c
Yusuke Oshiro <oshiro@jsk.imi.i.u-tokyo.ac.jp>
2015-12-12 14:31:23 +0900

    add powerline fonts

--- font-install.sh
+++ font-install.sh
@@ -12,11 +12,21 @@ wget http://iij.dl.sourceforge.jp/mix-mplus-ipa/59022/migu-1m-20130617.zip
 unzip migu-1m-20130617.zip
 rm migu-1m-20130617.zip
 git clone https://github.com/yascentur/Ricty.git
-# git clone https://github.com/Lokaltog/powerline.git
-git clone https://github.com/powerline/fontpatcher.gi
+
+mkdir $GENFONTS/powerline
+cd $GENFONTS/powerline
+git clone https://github.com/powerline/fontpatcher.git
+git clone https://github.com/powerline/fonts.git
+./fonts/install.sh
+cd $HOME/.local/share/fonts
+for f in *.*tf
+do
+    mv -i "$f" "$(echo $f | sed -e 's/\s\+/_/g')"
+    cp "$(echo $f | sed -e 's/\s\+/_/g')" $HOME/.fonts
+done
 
 
-cd Ricty
+cd $GENFONTS/Ricty
 ./ricty_generator.sh $GENFONTS/Inconsolata.otf $GENFONTS/migu-1m-20130617/migu-1m-regular.ttf $GENFONTS/migu-1m-20130617/migu-1m-bold.ttf
 mv Ricty*.ttf $HOME/.fonts
 fc-cache -fv
@@ -25,8 +35,8 @@ cd $GENFONTS/powerline
 
 
 cd $GENFONTS
-fontforge -script $GENFONTS/fontpatcher/scripts/powerline-fontpatcher $HOME/.fonts/Ricty-Regular.ttf
-fontforge -script $GENFONTS/fontpatcher/scripts/powerline-fontpatcher $HOME/.fonts/Ricty-Bold.ttf
+fontforge -script $GENFONTS/powerline/fontpatcher/scripts/powerline-fontpatcher $HOME/.fonts/Ricty-Regular.ttf
+fontforge -script $GENFONTS/powerline/fontpatcher/scripts/powerline-fontpatcher $HOME/.fonts/Ricty-Bold.ttf
 
 for f in *.ttf
 do
