9c701c1d72026c3803b4701bb598daeef706a06e
oshiro <oshiro@jsk.imi.i.u-toyko.ac.jp>
2016-04-22 17:54:41 +0900

    resolve conflict

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.dotfiles/inputrc $HOME/.inputrc
+ln -sf $HOME/.dotfiles/Xdefaults $HOME/.Xdefaults
 
 ln -sf $HOME/.dotfiles/peco $HOME/.peco
 ln -sf $HOME/.dotfiles/percol.d $HOME/.percol.d
--- zsh/ros.zsh
+++ zsh/ros.zsh
@@ -1,4 +1,6 @@
 #!usr/bin/env zsh
+# -*- coding:utf-8 -*-
+
 ## ROS DISTRO
 #source $HOME/ros/groovy/setup.bash
 #source $HOME/ros/hydro/devel/setup.zsh
@@ -65,9 +67,7 @@ function rossethrp2018() {
 #       cd ~/ros/hydro/devel
 #       return 0
 #     fi
-
 #     _ros_decode_path $1 forceeval
-
 #     if [ $? != 0 ]; then
 #       echo "roscd: No such package '$1'"
 #       return 1
@@ -79,3 +79,24 @@ function rossethrp2018() {
 #       return 0
 #     fi
 # }
+
+# overwrite update_prompt for rossetmaster
+_update_prompt() {
+    local master_host=$(echo $ROS_MASTER_URI | cut -d\/ -f3 | cut -d\: -f1)
+    if [ "$master_host" = "localhost" ]; then
+        if echo $PS1 | grep "\[http://.*\]" > /dev/null
+        then
+            export PS1="${WITHOUT_ROS_PROMPT}"
+        fi
+    elif [ "$master_host" != "" ]; then
+        local ros_prompt="[$ROS_MASTER_URI][$ROS_IP]"
+        if [ "$CATKIN_SHELL" = "bash" ]; then
+            export PS1="\[\033[00;31m\]$ros_prompt\[\033[00m\] ${WITHOUT_ROS_PROMPT}"
+        elif [ "$CATKIN_SHELL" = "zsh" ]; then
+            export PS1="%{$fg[black]%}%{$bg[green]%}[$ROS_MASTER_URI] %{$reset_color%}%{$fg[green]%}%{$bg[magenta]%}"" %{$reset_color%}%{$fg[black]%}%{$bg[magenta]%}[$ROS_IP] %{$reset_color%}%{$fg[magenta]%}"" %{$reset_color%}
+%{$reset_color%}${WITHOUT_ROS_PROMPT}"
+        else
+            echo "unsupported shell"
+        fi
+    fi
+}
