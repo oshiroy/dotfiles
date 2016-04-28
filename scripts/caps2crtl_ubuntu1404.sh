fcb8b11f6a21dd214385a664a53a68e22847b59b
oshiro <la.fete.du.travail@gmail.com>
2016-04-28 17:18:25 +0900

    add script for capslock2ctrl

---
+++ scripts/caps2crtl_ubuntu1404.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+dconf reset /org/gnome/settings-daemon/plugins/keyboard/active
+dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+
+echo "setting is finished. Please reboot."
