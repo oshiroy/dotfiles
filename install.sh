f93face277e343e07c22177829da63944a8a3fd2
oshiro <oshiro@jsk.imi.i.u-toyko.ac.jp>
2016-04-29 20:48:33 +0900

    add vimrc

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.dotfiles/inputrc $HOME/.inputrc
 ln -sf $HOME/.dotfiles/Xdefaults $HOME/.Xdefaults
+ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
 
 ln -sf $HOME/.dotfiles/peco $HOME/.peco
 ln -sf $HOME/.dotfiles/percol.d $HOME/.percol.d
---
+++ vimrc
@@ -0,0 +1,14 @@
+
+syntax on
+
+set encoding=utf-8
+
+set autoindent
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set cursorline
+set number
+set ruler
+set nobackup
+set expandtab
